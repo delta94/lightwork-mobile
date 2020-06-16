8d350114c410a403576411670283ed79d72c4e20
Apollon Seven <apollonseven@gmail.com>
2020-06-17 00:18:24 +0300

    Update

--- app/theme/fonts/index.ts
+++
@@ -1,8 +0,0 @@
-import * as Font from "expo-font"
-
-export const initFonts = async () => {
-  await Font.loadAsync({
-    Lato: require("./Lato-Regular.ttf"),
-    "Lato-Regular": require("./Lato-Regular.ttf"),
-  })
-}
