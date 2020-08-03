c892ffc215ba508de1200fd1dda32ab30a543875
Apollon Linux <apollonseven@gmail.com>
2020-08-03 05:33:11 +0300

    Add rn-yamap

--- app/components/MapScreen/Map/Map.tsx
+++ app/components/MapScreen/Map/Map.tsx
@@ -1,18 +1,21 @@
 import React from "react"
 import { ViewStyle, View } from 'react-native';
+import YaMap, { Marker } from 'react-native-yamap';
+
 
 const CONTAINER: ViewStyle = {
 
 }
 
 const Map = () => {
+    YaMap.init('c9fe7024-671e-49c8-945c-4b9a87c59902');
     return (
-        <View
-                style={{
-                    flex: 1,
-                }}
-            >
-        </View>
+        <YaMap
+        userLocationIcon={{ uri: 'https://www.clipartmax.com/png/middle/180-1801760_pin-png.png' }}
+        showUserPosition={true}
+        style={{ flex: 1, marginBottom: 40, }}
+      ><Marker point={{lat: 50, lon: 50}}/>
+      </YaMap>
     )
 }
 
