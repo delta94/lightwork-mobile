96eba724a28c4a63f2bcab4be79b52c64cd95663
Apollon Linux <apollonseven@gmail.com>
2020-08-05 07:14:21 +0300

    Update Map

--- app/components/MapScreen/Map/Map.tsx
+++ app/components/MapScreen/Map/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import { ViewStyle, View } from 'react-native';
 import YaMap, { Marker } from 'react-native-yamap';
 import IMAGES from '../../../constants/images';
@@ -11,17 +11,29 @@ const Map = () => {
     YaMap.init('c9fe7024-671e-49c8-945c-4b9a87c59902');
     YaMap.setLocale('ru_RU');
     const YA_MAP = useRef(null);
+    const [activePoint, setPoint] = useState({"lat": 0, "lon": 0});
+    const _onMapPress = (p: any) => {
+      setPoint(p);
+      console.log(activePoint)
+      YA_MAP.current.setCenter(p, 10, 0, 0, 0.5)
+    }
+
+    const _onLongMapPress = (p: any) => {
+      console.log(p)
+      setPoint(p);
+    }
     return (
-      
-          <YaMap
+      <YaMap
         ref={YA_MAP}
         userLocationIcon={IMAGES.main.card }
         showUserPosition={true}
         userLocationAccuracyFillColor="#000"
+        onMapPress={(point) => {_onMapPress(point.nativeEvent)}}
+        onMapLongPress={(point) => {_onLongMapPress(point.nativeEvent)}}
         style={{ flex: 1, marginBottom: 40, }}
-      ><Marker point={{lat: 50, lon: 50}}/>
+      >
+        <Marker point={activePoint}/>
       </YaMap>
-        
     )
 }
 
