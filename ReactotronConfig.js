4ae988be090a5bd4568aeac55c6bd77057924838
Apollon Linux <apollonseven@gmail.com>
2020-08-20 04:54:14 +0300

    Add reactotron

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,13 +1,8 @@
-import Reactotron from 'reactotron-react-native';
+import Reactotron from 'reactotron-react-native'
 import AsyncStorage from '@react-native-community/async-storage';
-import {reactotronRedux} from 'reactotron-redux';
 
-const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+Reactotron
+  .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
   .configure() // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
-  .use(reactotronRedux())
-  .connect(); // let's connect!
-
-console.tron = Reactotron;
-
-export default reactotron;
+  .connect() // let's connect!
